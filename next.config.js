1c664ec76954fccabd913e8bf20269bb256318a9
tamadakrishna <tamadakrishnaa@gmail.com>
2024-02-11 20:58:36 +0530

    ecom

--- next.config.js
+++ next.config.js
@@ -1,19 +1,19 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
    
     env:{
-        API_URL: "http://localhost:3000",
-        DB_URI:"mongodb+srv://admin:admin@cluster0.uskgjtq.mongodb.net/?retryWrites=true&w=majority",
-        NEXTAUTH_SECRET: "codingwithabbas",
+        // https://deploy-ebon-theta.vercel.app
+        // API_URL: "http://localhost:3000",
+        // DB_URI:"mongodb+srv://admin:admin@cluster0.uskgjtq.mongodb.net/?retryWrites=true&w=majority",
+        // NEXTAUTH_SECRET: "krishna",
 
-        STRIPE_PUBLIC_KEY: "pk_test_51ONubaSHZfLMCurbIEJZC9AiaonKjBRzwssg5DcmcG6rnwQgr6SG4XBFC9dfNELAlNANLZ76aTto0NAfyv74DjmU007uLUFkF7",
-        STRIPE_PRIVATE_KEY: "sk_test_51ONubaSHZfLMCurb8depsFhnl1MMZxYeKLmosyyZgumfrfKzU5fW7pUUkxUH3w7jV3pyxiNiSml2mgqhBrjyVZQF00tcu0KMwO",
+        // STRIPE_PUBLIC_KEY: "pk_test_51ONubaSHZfLMCurbIEJZC9AiaonKjBRzwssg5DcmcG6rnwQgr6SG4XBFC9dfNELAlNANLZ76aTto0NAfyv74DjmU007uLUFkF7",
+        // STRIPE_PRIVATE_KEY: "sk_test_51ONubaSHZfLMCurb8depsFhnl1MMZxYeKLmosyyZgumfrfKzU5fW7pUUkxUH3w7jV3pyxiNiSml2mgqhBrjyVZQF00tcu0KMwO",
 
-        STRIPE_WEBHOOK_SECRET: "whsec_c7948d152d7f805b161e7d9b4617434241d8fba5f232a2be6ae11f79e919d946",
+        // STRIPE_WEBHOOK_SECRET: "whsec_c7948d152d7f805b161e7d9b4617434241d8fba5f232a2be6ae11f79e919d946",
 
-        CLOUD_NAME: "dewzsspgu",
-        CLOUDINARY_API_KEY: "611878661422294",
-        CLOUDINARY_API_SECRET: "emnAK4KUuAdfimtHrQSZHMdTqag",
+        // CLOUD_NAME: "dewzsspgu",
+        // CLOUDINARY_API_KEY: "611878661422294",
+        // CLOUDINARY_API_SECRET: "emnAK4KUuAdfimtHrQSZHMdTqag",
     },
     images: {
         domains: ["res.cloudinary.com"],
