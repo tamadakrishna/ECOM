17a46cb7bf798119fd98f01a3c8b998b36a47617
tamadakrishna <tamadakrishnaa@gmail.com>
2024-02-12 16:40:44 +0530

    ecom

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ const nextConfig = {
    
     env:{
         // https://deploy-ebon-theta.vercel.app
-        API_URL: "http://localhost:3000",
+        // API_URL: "http://localhost:3000",
         // DB_URI:"mongodb+srv://admin:admin@cluster0.uskgjtq.mongodb.net/?retryWrites=true&w=majority",
         // NEXTAUTH_SECRET: "krishna",
 
