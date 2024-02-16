58d6037a0427b380d1017b3d7a5e655b1ddf6b88
tamadakrishna <tamadakrishnaa@gmail.com>
2024-02-17 00:18:09 +0530

    admin

--- next.config.js
+++ next.config.js
@@ -19,6 +19,10 @@ const nextConfig = {
     images: {
         domains: ["res.cloudinary.com"],
       },
+      typescript: {
+
+        ignoreBuildErrors: true,
+      },
 }
 
 module.exports = nextConfig
