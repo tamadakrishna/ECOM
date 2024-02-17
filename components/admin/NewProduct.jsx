54029f197bb4de62ba161ef13797ee93e5180308
tamadakrishna <tamadakrishnaa@gmail.com>
2024-02-17 20:06:57 +0530

    style

--- components/admin/NewProduct.jsx
+++ components/admin/NewProduct.jsx
@@ -62,7 +62,7 @@ const NewProduct = () => {
           <label className="block mb-1 text-[#1e1d1d]"> Name </label>
           <input
             type="text"
-            className="relative m-0 placeholder:text-[#1e1d1d] w-full block flex-auto rounded border border-solid border-neutral-300 bg-transparent bg-clip-padding px-3 py-[0.25rem] text-base font-normal leading-[1.6]  outline-none transition duration-200 ease-in-out focus:z-[3] focus:border-primary focus:text-neutral-700 focus:shadow-[inset_0_0_0_1px_rgb(59,113,202)] focus:outline-none dark:border-neutral-600 dark:text-neutral-200 dark:placeholder:text-neutral-200 dark:focus:border-primary"
+            className="relative m-0 placeholder:text-[#1e1d1d] w-full block flex-auto rounded border border-solid border-neutral-300  bg-clip-padding px-3 py-[0.25rem] text-base font-normal leading-[1.6]  outline-none transition duration-200 ease-in-out focus:z-[3] focus:border-primary focus:text-neutral-700 focus:shadow-[inset_0_0_0_1px_rgb(59,113,202)] focus:outline-none"
             placeholder="Product name"
             name="name"
             value={name}
